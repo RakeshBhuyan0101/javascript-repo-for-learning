4481aba9e25634bc8faf0a10ba0f34835e50d0db
RakeshBhuyan0101 <109802382+RakeshBhuyan0101@users.noreply.github.com>
2024-05-28 15:01:08 +0000

    operation

---
+++ 1_basics/01_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = 44
+
+let convertToString = String(score)
+console.log(typeof"convertToString")
+console.log(typeof(score))
+
+
+// 33 = 33
+// 33ab = NaN
+// true = 1 or fasle = 0
+let isLoggedIn = 1;
+
+let boolIsLoggedIn = String(isLoggedIn)
+console.log(typeof boolIsLoggedIn)
+console.log (boolIsLoggedIn)
+
+// 1 -> true 0->false
+// "" -> false
+// "rakesh" = true;
+
+let someNumber = 3
+let stringSomeNumber = String(someNumber)
+console.log(typeof stringSomeNumber)
+console.log(stringSomeNumber)
+
+                // OPERATIONS
+
+let val = 4;
+let negval = -val
+console.log(negval)
+
+let str1 = "Rakesh"
+let str2 = "Bhuyan"
+let concate = str1+str2
+console.log(concate)
+
+console.log("1" + 2)
+console.log( 1 + "2" )
+console.log("1" + 2 +3)
+console.log(1 + 2 + "3" )
