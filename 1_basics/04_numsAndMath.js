d958a5328146f34c86ef2e91acc11092a1ce146d
RakeshBhuyan0101 <109802382+RakeshBhuyan0101@users.noreply.github.com>
2024-06-03 15:00:44 +0000

    date and math

---
+++ 1_basics/04_numsAndMath.js
@@ -0,0 +1,35 @@
+const score = 400
+
+const balance = new Number(456)
+console.log(balance.toString())
+console.log(score.toFixed(3))
+
+const othernumber = 23.39373
+console.log(othernumber.toPrecision(1));
+
+const hundred = 10000000
+console.log(hundred.toLocaleString('en-IN'))
+
+
+
+                // ----------- Mathes ---------------
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.66))
+// console.log(Math.floor(4.2))
+// console.log(Math.ceil(4.1))
+// console.log( Math.max(4,1,3,34) )
+// console.log( Math.min(4,1,3,34) )
+
+
+console.log( Math.random())
+console.log( Math.floor(Math.random()*10) + 1 )
+const min = 20
+const max = 40
+
+console.log(Math.ceil(Math.random()* ( max-min+1 )) + min);  // formula for get random value form min to max range
+
+
+
+
---
+++ 1_basics/05_dateAndTime.js
@@ -0,0 +1,23 @@
+let myDate = new Date() // date is a object
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString);
+
+// const myCreatedDate = new Date(2023,0,23)
+// const myCreatedDate1 = new Date(2023,0,23,3,5,34)
+// const myCreatedDate2 = new Date("2023-01-22")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate1.toLocaleDateString());
+// console.log(myCreatedDate2.toLocaleString());
+
+
+                // TIME STAMPS 
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getDay())
