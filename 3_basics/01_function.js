939ea098537d497e0470e1297b6f70b404e39080
RakeshBhuyan0101 <109802382+RakeshBhuyan0101@users.noreply.github.com>
2024-06-06 02:51:47 +0000

    scopes in js

--- 3_basics/01_function.js
+++ 3_basics/01_function.js
@@ -28,3 +28,34 @@ function loginUserName1 (userName = "Sam") {
 loginUserName()
  const user = loginUserName("Rakesh")
  console.log(user)
+
+ function calulateCartPrice (val1 , val2 , ...num1 ) { // rest operator
+    return num1
+ }
+
+//  console.log(calulateCartPrice(23 ,45,44,22,44)) // return an array
+
+const users = {
+    userName : "munna",
+    price : 992
+}
+// pass object in function parameter
+function handelObject (anyObject) {
+    console.log(`user name is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+
+handelObject(users)
+handelObject({
+    userName : "sam",
+    price : 2224
+})
+
+
+// pass arr in function paramenter
+const newArray = [100, 200, 300, 400]
+
+function returnSecondElement (arr){
+    return newArray[1]
+}
+
+console.log(returnSecondElement(newArray))
